35617dc2bbca52c8ed28ec6be80f4d53d4a76575
anurag10 <anuragsoni39@yahoo.com>
2020-10-03 19:05:11 +0530

    revise work break

--- WordBreak.java
+++ WordBreak.java
@@ -1,3 +1,19 @@
+/*
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+    The same word in the dictionary may be reused multiple times in the segmentation.
+    You may assume the dictionary does not contain duplicate words.
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+*/
+
 class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
         
